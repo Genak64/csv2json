0db9f7d9afe9d4030ae26c098ad1d31938870277
Genak64 <genak64@ukr.net>
2021-10-08 00:10:22 +0300

    Upd parser

---
+++ csv2json/ParseCSV.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csv2json
+{
+    static class ParseCSV
+    {
+
+
+        public static table loadCsvFile(string path, string fileEncoding, char separator)
+        {
+            //Кодировка файла
+            //string fileEncoding = "Windows-1251";
+            //string path = "products.csv";
+            //char separator = ';';
+
+            table listStr = new table();
+
+            List<string> fileLine = new List<string>();
+
+            fileLine = readCsvFile(path, fileEncoding);
+
+            List<string> str = new List<string>();
+
+            foreach (string line in fileLine)
+            {
+                str = pCsvString(line, separator);
+                listStr.item.Add(new List<string>(str));
+            }
+
+            return listStr;
+        }
+
+
+        static List<string> readCsvFile(string pathFile, string encodingFile)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            Encoding enc = Encoding.GetEncoding(encodingFile);
+
+            // Encoding utf8 = Encoding.GetEncoding("UTF-8");
+            // Encoding win1251 = Encoding.GetEncoding("Windows-1251");
+
+            List<string> fileLine = new List<string>();
+
+            using (StreamReader reader = new StreamReader(pathFile, enc))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    fileLine.Add(line);
+                    Console.WriteLine(line);  //***************************
+                }
+            }
+            return fileLine;
+        }
+
+
+        //возвращает csv-строку разбитую на отдельные элементы разделителем separator
+        static List<string> pCsvString(string csvString, char separator)
+        {
+            List<string> item = new List<string>();
+
+            string[] s = new string[2];
+            s[1] = csvString;
+
+            while (s[1] != "")
+            {
+                s = fieldParse(csvString, separator);
+                s[0] = s[0].Replace("\"\"", "\"");
+                item.Add(s[0]);
+                csvString = s[1];
+            }
+            return item;
+
+        }
+
+        //вырезает и возвращает отдельный элемент csv-строки в outString[0]
+        //и остальную часть строки в outString[1]
+        static string[] fieldParse(string text, char separator)
+        {
+            string[] outString = new string[2];
+            int i = 0;
+            if (text == null)
+            {
+                outString[1] = "";
+                return outString;
+            }
+            if (text.IndexOf(separator) == -1 && text.IndexOf("\"") == -1)
+            {
+                outString[0] = text;
+                outString[1] = "";
+                return outString;
+            }
+            if (text.IndexOf("\"") == 0 && text.IndexOf("\"", 1) == text.Length - 1)
+            {
+                outString[0] = text.Substring(1, text.Length - 2);
+                outString[1] = "";
+                return outString;
+            }
+            if (text[i] != separator && text[i] != '"' && i == 0)
+            {
+                outString[0] = text.Substring(0, text.IndexOf(separator.ToString(), i));
+                outString[1] = text.Substring(text.IndexOf(separator.ToString(), i) + 1);
+                return outString;
+            }
+            if (text[i] == separator && i == 0)
+            {
+                outString[0] = " ";
+                outString[1] = text.Substring(1);
+                return outString;
+            }
+            if (text[i] == '"' && i == 0)
+            {
+                while (i < text.Length)
+                {
+                    if (text.IndexOf(("\"\"\"\"" + separator.ToString()), i) != -1)
+                    {
+                        i = text.IndexOf(("\"\"\"\"" + separator.ToString()), i) + 3;
+                        continue;
+                    }
+                    if (text.IndexOf(("\"\"\"" + separator.ToString()), i) != -1)
+                    {
+                        outString[0] = text.Substring(1, text.IndexOf(("\"" + separator.ToString()), i) - 1);
+                        outString[1] = text.Substring(text.IndexOf(("\"" + separator.ToString()), i) + 2);
+                        return outString;
+                    }
+                    if (text.IndexOf("\";", i) != -1 && text[(text.IndexOf(("\"" + separator.ToString()), i) - 1)] != '"')
+                    {
+                        outString[0] = text.Substring(1, text.IndexOf("\";", i) - 1);
+                        outString[1] = text.Substring(text.IndexOf(("\"" + separator.ToString()), i) + 2);
+                        return outString;
+                    }
+                    i++;
+                }
+            }
+            return outString;
+        }
+
+
+
+    }
+}
--- csv2json/Program.cs
+++ csv2json/Program.cs
@@ -14,186 +14,47 @@ namespace csv2json
             //подключаем украинский язык в консоли
             //Console.OutputEncoding = System.Text.Encoding.Unicode;
             //Console.InputEncoding = System.Text.Encoding.Unicode;
+            if (args.Length == 0)
+            {
+                Console.WriteLine(" отсутствуют аргументы, варианты:");
+                Console.WriteLine(" /имя файла/");
+                Console.WriteLine(" /имя файла/ /кодировка/");
+                Console.WriteLine(" /имя файла csv/ /кодировка/ /разделитель/");
+                Console.WriteLine(" например: csv2json file.csv Windows-1251 ;");
+                return;
+            }
 
-            /*
+            if (args.Length==1)
+            {
 
+
+            }
+
+            //Кодировка файла
+            string fileEncoding = "Windows-1251";
             string path = "products.csv";
-            List<string> fileLine = new List<string>();
+            char separator = ';';
 
-            fileLine = readCsvFile(path, "Windows-1251");
+            table tableItems = new table();
 
-
-
+            tableItems =ParseCSV.loadCsvFile(path, fileEncoding, separator);
+                       
+        }
 
             
 
-            List<string> str = new List<string>();
-
-            foreach (string line in fileLine)
-            {
-                str = parseCsvString(line, ';');
-
-                foreach (string s in str) Console.WriteLine(s);
-
-            }
-          */
-
-
-            string text = "111;\"113\"\"\"\";23123\"\"\";23end\";\"123\";123";
-
-            List<string> str = new List<string>();
-
-            string[] s = new string[2];
-            s[1] = text;
-            while (s[1]!= "")
-            {
-               s = fieldParse(text);
-               str.Add(s[0]);
-                text = s[1];
-                Console.WriteLine(s[0]);
-            }
-
-         
-
-            foreach (string st in str) Console.WriteLine(st);
-        }
-
-        static string[] fieldParse(string text)
-        {
-            string[] outString = new string[2];
-
-            int i = 0;
-            if (text.IndexOf(";")==-1)
-            {
-                outString[0] = text;
-                outString[1] = "";
-                return outString;
-            }
-
-            if (text[i] != ';' && text[i] != '"' && i == 0)
-            {
-            //    Console.WriteLine(text.Substring(0, text.IndexOf(";", i)));
-                outString[0]=text.Substring(0, text.IndexOf(";", i));
-                outString[1] = text.Substring(text.IndexOf(";", i) + 1);
-                return outString;
-            }
-
-            if (text[i] == ';' && i == 0)
-            {
-            //    Console.WriteLine("empty field");
-                outString[0]=" ";
-                outString[1] = text.Substring(1);
-                return outString;
-            }
-
-            if (text[i] == '"' && i == 0)
-            {
-                while (i < text.Length)
-                {
-                    if (text.IndexOf("\";", i) != -1 && text[(text.IndexOf("\";", i) - 1)] != '"')
-                    {
-              //          Console.WriteLine(text.Substring(1, text.IndexOf("\";", i) - 1));
-                        outString[0]=text.Substring(1, text.IndexOf("\";", i) - 1);
-                        outString[1] = text.Substring(text.IndexOf("\";", i) + 2);
-                        return outString;
-                        
-                    }
-                    i++;
-                }
-            }
-
-            return outString;
-        }
-
-
-        static List<string> parseCsvString(string csvString, char separator)
-        {
-            List<string> str = new List<string>();
-
-            bool itemOpen = false;
-            int indexOpen = 0;
-            int indexClosed = 0;
-            int openQuotes = 0;
-            int indexQuotes = 0;
-            string quotes = "\"";
-
-            csvString = csvString.Insert(csvString.Length, separator.ToString());
-
-            for (int i = 0; i < csvString.Length; i++)
-            {
-
-                if (csvString[i] == '"' && itemOpen == false)
-                {
-                     openQuotes = i;
-                    if (csvString.IndexOf((quotes + quotes + separator), i) == -1)
-                    {
-                        
-                        indexQuotes = csvString.IndexOf((quotes + separator), i);
-                        str.Add(csvString.Substring(i + 1, indexQuotes - i - 1));
-                        i = indexQuotes;
-                    } else
-                    {
-                        Console.WriteLine((quotes + quotes + separator));
-
-                        i = csvString.IndexOf((quotes + quotes + separator), i)+3;
-                        indexQuotes = csvString.IndexOf((quotes + separator), i);
-                        str.Add(csvString.Substring(openQuotes + 1, indexQuotes - i - 1));
-                        i = indexQuotes;
-                    }
-
-                }
-
-                if (csvString[i] != '"' && itemOpen == false)
-                {
-                    itemOpen = true;
-                    indexOpen = i;
-                }
-                
-                if (csvString[i] == separator && itemOpen == true)
-                {
-                    indexClosed = i;
-                    str.Add(csvString.Substring(indexOpen, indexClosed - indexOpen));
-                    itemOpen = false;
-                    indexOpen = i;
-                }
-/*
-                if (csvString.Length - 1 == i && itemOpen == true)
-                {
-                    indexClosed = csvString.Length;
-                    str.Add(csvString.Substring(indexOpen, indexClosed - indexOpen));
-                }
-*/
-            }
-
-            return str;
-
-        }
-
-        static List<string> readCsvFile(string pathFile, string encodingFile)
-        {
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
-            Encoding enc = Encoding.GetEncoding(encodingFile);
-
-           // Encoding utf8 = Encoding.GetEncoding("UTF-8");
-           // Encoding win1251 = Encoding.GetEncoding("Windows-1251");
-
-            List<string> fileLine = new List<string>();
-
-            using (StreamReader reader = new StreamReader(pathFile, enc))
-            {
-                string line;
-                while ((line = reader.ReadLine()) != null)
-                {
-                    fileLine.Add(line);
-                    Console.WriteLine(line);
-                }
-            }
-            return fileLine;
-        }
-
-
-
-
     }
+
+      
+        class table
+        {
+          public  List<List<string>> item = new List<List<string>>();
+
+        }
+
+       
+
+
+
+   
 }
